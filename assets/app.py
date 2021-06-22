0045da85b57b77674f38ec1a0bdf82d83e5e3a1f
ramonmarrero <ramon.marrero@lanube.io>
2021-06-22 14:35:29 +0200

    latest changes

--- assets/app.py
+++ assets/app.py
@@ -1,5 +1,20 @@
 import sys
+import logging
+import sagemaker
+from time import gmtime, strftime
+from sagemaker.spark.processing import PySparkProcessor
 
 def handler(event, context):
-    return 'Hello from AWS Lambda using Python' + sys.version + '!'
-git
+
+
+    sagemaker_logger = logging.getLogger("sagemaker")
+    sagemaker_logger.setLevel(logging.INFO)
+    sagemaker_logger.addHandler(logging.StreamHandler())
+
+    sagemaker_session = sagemaker.Session()
+    bucket = sagemaker_session.default_bucket()
+    role = sagemaker.get_execution_role()
+
+    print(f'Bucket: {bucket} Role: {role}')
+
+    return 'AWS Lambda Spark Process Ended - ' + sys.version + '!'
